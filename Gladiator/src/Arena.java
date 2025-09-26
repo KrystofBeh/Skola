6626fcd6a6b199b40215c242f63588180c859f41
KrystofBeh <kacko896ytb@gmail.com>
2025-09-26 11:51:54 +0200

    Add files via upload

---
+++ Gladiator/src/Arena.java
@@ -0,0 +1,48 @@
+
+public class Arena {
+    private Gladiator redTeam;
+    private Gladiator blueTeam;
+
+    public void simulateOneRound() {
+        int blueUtok = (int)(Math.random() * (blueTeam.getMaxDamage() - blueTeam.getMinDamage()) + blueTeam.getMinDamage());
+        int blueDefence = (int)(Math.random() * (blueTeam.getMaxDefence() - blueTeam.getMinDefence()) + blueTeam.getMinDefence());
+
+        int redUtok = (int)(Math.random() * (redTeam.getMaxDamage() - redTeam.getMinDamage()) + redTeam.getMinDamage());
+        int redDefence = (int)(Math.random() * (redTeam.getMaxDefence() - redTeam.getMinDefence()) + redTeam.getMinDefence());
+
+        if (redUtok > blueDefence) {
+            blueTeam.setHp(blueTeam.getHp() - (redUtok  - blueDefence));
+            System.out.println("red ubral: " + (redUtok - blueDefence) + ", blue HP: " + blueTeam.getHp());
+        }else {
+            System.out.println("red missnul");
+        }
+        if (blueUtok > redDefence) {
+            redTeam.setHp(redTeam.getHp() - (blueUtok  - redDefence));
+            System.out.println("blue ubral: " + (blueUtok - redDefence) + ", red HP: " + redTeam.getHp());
+        }else{
+            System.out.println("blue missnul");
+        }
+
+        if (blueTeam.getHp() <= 0) {
+            System.out.println("red vyhral");
+        }
+        if (redTeam.getHp() <= 0) {
+            System.out.println("blue vyhral");
+        }
+
+    }
+
+    public void simulateRounds(){
+        while (blueTeam.getHp() > 0 && redTeam.getHp() > 0) {
+            simulateOneRound();
+        }
+    }
+
+    public void addRedTeam(Gladiator gladiator) {
+        redTeam = gladiator;
+    }
+
+    public void addBlueTeam(Gladiator gladiator) {
+        blueTeam = gladiator;
+    }
+}
