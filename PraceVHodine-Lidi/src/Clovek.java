b591e786485f562fb6aad8cc79bcec793822e293
KrystofBeh <kacko896ytb@gmail.com>
2025-10-03 11:19:26 +0200

    Add files via upload

---
+++ PraceVHodine-Lidi/src/Clovek.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Clovek {
+    private String jmeno;
+    private String prijmeni;
+    private int vek;
+    private String pohlavi;
+    private ArrayList<Clovek> sourozenci = new ArrayList<>();
+
+    public Clovek(String jmeno, String prijmeni, int vek, String pohlavi) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.vek = vek;
+        this.pohlavi = pohlavi;
+    }
+
+    public void addSourozenec(Clovek clovek){
+        for (Clovek c : sourozenci){
+            if (!clovek.sourozenci.contains(c)){
+                c.sourozenci.add(clovek);
+                clovek.bylPridan(c);
+            }
+        }
+
+
+        sourozenci.add(clovek);
+        clovek.bylPridan(this);
+    }
+
+    private void bylPridan(Clovek clovek){
+        sourozenci.add(clovek);
+    }
+
+    public int getPocetSourozencu(){
+        return sourozenci.size();
+    }
+
+}
