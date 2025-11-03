f24df26df611f616f64b2c566a96ad345bf3a111
KrystofBeh <kacko896ytb@gmail.com>
2025-11-03 10:54:11 +0100

    Add files via upload

---
+++ Sem-Slozity/core/src/main/java/io/github/some_example_name/Parts/Ptacek.java
@@ -0,0 +1,70 @@
+
+package io.github.some_example_name.Parts;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Polygon;
+
+public class Ptacek {
+
+    private final Sprite sprite;
+    private float speed;
+
+    public Ptacek(Texture texture) {
+        this.sprite = new Sprite(texture);
+
+        sprite.setSize(70f, 50f); //sprite setup
+        sprite.setOriginCenter();
+        sprite.setPosition(200f, 640f);
+        sprite.setRotation(0f);
+
+        this.speed = 0f;
+    }
+
+    public void update(){
+
+        if(getY() + sprite.getHeight() > 960) { //checks for top collisions
+            setY(960 - sprite.getHeight());
+            speed = 0f;
+        }
+
+        if(sprite.getRotation()>-40f){ //rotates the bird
+            setRotation(getRotation() - 0.6f);
+        }
+
+        speed += Gdx.graphics.getDeltaTime() * 5f; //falling speed
+        setY(getY() - speed);
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) { //check for jump
+            speed = -5f;
+            setRotation( 40f );
+        }
+
+        if (sprite.getY() < 0 - sprite.getHeight()) {
+            System.exit(0);
+        }
+    }
+
+    private float getRotation() {
+        return sprite.getRotation();
+    } //makes code cleaner
+
+    private void setRotation(float rotation) {
+        sprite.setRotation(rotation);
+    }
+
+    private void setY(float y){
+        sprite.setY(y);
+    }
+
+
+    public float getY() {
+        return sprite.getY();
+    } //actual getters
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+}
---
+++ Sem-Slozity/core/src/main/java/io/github/some_example_name/managers/SpriteManager.java
@@ -0,0 +1,119 @@
+package io.github.some_example_name.managers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import io.github.some_example_name.Parts.Ptacek;
+import io.github.some_example_name.Parts.Tube;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SpriteManager {
+    public static SpriteManager instance; // Proměnná pro instanci singletonu /** * Get method for singleton of SpriteManager * @return SpriteManager instance */
+    private ShapeRenderer shapeRenderer;
+    private static SpriteManager getInstance() {
+        if (instance == null) {
+            instance = new SpriteManager();
+        }
+        return instance;
+    }
+
+    public ArrayList<Tube> trubky = new ArrayList<>();
+
+    public SpriteBatch batch;
+    public HashMap<String, Texture> textures;
+    private int pocetTrubek = 1000 ;
+    private final Ptacek ptak;
+
+
+    public SpriteManager() {
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
+        textures = new HashMap<>();
+        setTextures();
+        ptak = new Ptacek(textures.get("Duck"));
+    }
+
+    private void setTextures() {
+        textures.put("Tube", new Texture("Green_pipe.png"));
+        textures.put("Duck", new Texture("duck.png"));
+    }
+
+    public void addTrubice(){
+        trubky.add(new Tube(textures.get("Tube")));
+    }
+
+    public void checkTrubice(Tube trubice){
+        Sprite s = trubice.getSprite();
+        Sprite p = ptak.getSprite();
+
+        float x = s.getX();
+        float y = s.getY();
+        float w = s.getWidth();
+        float h = s.getHeight();
+
+        Rectangle spodni = new Rectangle(x, y - 150 , w, h / 2f);
+        Rectangle horni = new Rectangle(x, y + h / 2f + 150, w, h / 2f);
+        Rectangle ptakRcl = new Rectangle(new Sprite(new Texture("duck.png")));
+        ptakRcl.height /= 2f;
+        ptakRcl.width /= 2f;
+
+
+        // debug - vykreslení hitboxů
+        batch.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.rect(spodni.x, spodni.y, spodni.width, spodni.height);
+        shapeRenderer.rect(horni.x, horni.y, horni.width, horni.height);
+        shapeRenderer.rect(ptakRcl.x, ptakRcl.y, ptakRcl.width, ptakRcl.height);
+        shapeRenderer.end();
+        batch.begin();
+
+        // kolize
+        if (ptakRcl.overlaps(spodni) || ptakRcl.overlaps(horni)) {
+            System.out.println("Game Over!");
+            System.exit(0);
+        }
+
+    }
+
+    public void update() {
+
+        ptak.update();
+
+
+        for (Tube x : trubky){
+            x.update(-2.5f);
+            draw(x.getSprite());
+            checkTrubice(x);
+        }
+
+        pocetTrubek++;
+        if (pocetTrubek > 300){
+            pocetTrubek = 0;
+            addTrubice();
+        }
+
+        draw(ptak.getSprite());
+    }
+
+    public void draw(Sprite sprite) {
+        sprite.draw(batch);
+    }
+
+
+    public void begin() {
+        batch.begin();
+    }
+
+    public void end() {
+        batch.end();
+    }
+
+    public void dispose() {
+        batch.dispose();
+    }
+}
