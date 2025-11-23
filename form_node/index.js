1b742dc6bda0f3ad31339c366c3d0fa1caea0a0e
KrystofBeh <kacko896ytb@gmail.com>
2025-11-23 11:38:12 +0100

    Add files via upload

--- form_node/index.js
+++ form_node/index.js
@@ -1,6 +1,51 @@
 let users = [];
 
-console.log("Parek");
+function pridejUser() {
+    let firstname = document.getElementById("firstName").value;
+    let lastname = document.getElementById("lastName").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+    let output = document.getElementById("output");
+
+    if (!firstname || !lastname || !email || !age) {
+        output.innerHTML = "neco je prazdne, prosim vyplnte to";
+        return;
+    }
+
+    for (let user of users) {
+        if (user.email === email) {
+            output.innerHTML = "email je jiz pouzity, zkuste jiny";
+            return;
+        }
+    }
+
+    users.push(new User(firstname, lastname, email, age));
+    console.log(users.at(users.length-1));
+}
+
+function prectiUsers(){
+    let seznam = "";
+    let vysledek = document.getElementById("vysledek");
+    for(let user of users){
+        console.log(user.toString());
+        seznam += user.toString() + "<br>";
+    }
+    vysledek.innerHTML = seznam;
+}
+
+function odstranUser(){
+    let deletEmail = document.getElementById("deleteEmail").value;
+    
+    for(let user of users){
+        if(user.email === deletEmail){
+            users.splice(users.indexOf(user), 1);
+            console.log("Byl odstranen, zbytek: ");
+            prectiUsers();
+            return;
+        }
+    }
+    console.log("tenhle email neni v zapsany")
+}
 
 class User {
     constructor(firstname, lastname, email, age) {
@@ -11,17 +56,6 @@ class User {
     }
 
     toString() {
-        return `Name: ${this.firstname} ${this.lastname}, Age: ${this.age}, Email ${this.email}`;
+        return `Name: ${this.firstname} ${this.lastname}, Age: ${this.age}, E-mail: ${this.email}`;
     }
-}
-
-function addUser() {
-    event.preventDefault();
-    let firstname = document.getElementById('firstName').value;
-    let lastname = document.getElementById('lastName').value;
-    let email = document.getElementById('email').value;
-    let age = document.getElementById('age').value;
-
-    users.push(new User(firstname, lastname, email, age));
-    console.log(users.at(users.length-1));
 }
